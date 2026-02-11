62c1c8b6276dee5d91113075c4a8cf6a96c37364
Maciej Kwasek <maciej.kwasek@elester-pkp.com.pl>
2026-02-11 13:02:03 +0100

    add minor improvements

--- rtl/ws2812b_drv.vhd
+++ rtl/ws2812b_drv.vhd
@@ -4,14 +4,16 @@ use ieee.std_logic_1164.all;
 entity ws2812b_drv is
 	generic
 	(
-		LED_NUMBER : natural := 64; -- 8x8 matrix
+		LED_NUMBER : natural := 8; -- 8x8 matrix
 		
+		REFRESH_PERIOD : natural := 1_000_000; -- 20ms @ 50MHz
+
 		T_RESET : natural := 4500;	-- 90us
 	
 		T0H : natural := 20; -- 400ns
 		T1H : natural := 40; -- 800ns
 		T0L : natural := 42; -- 840ns
-		T1L : natural := 22	-- 440ns	
+		T1L : natural := 22	-- 440ns
 	);
 	
 	port
@@ -29,7 +31,11 @@ architecture rtl of ws2812b_drv is
 
 	signal frame_buffer : frame_buffer_t :=
 	(
-		others => x"000000"
+		0 => x"ff0000",
+		1 => x"00ff00",
+		2 => x"0000ff",
+		3 => x"112233",
+		others => x"00ff00"
 	);
 
 	type drv_state_t is (IDLE, RESET_PULSE, LOAD_BIT, BIT_H, BIT_L);
@@ -59,11 +65,12 @@ begin
 
 			case c_state is
 				when IDLE =>				
-					if delay_cnt = 1_000_000 then
+					if delay_cnt = REFRESH_PERIOD then
 						c_state <= RESET_PULSE;
 						delay_cnt <= 0;
 						led_idx <= 0;
 						bit_idx <= 0;
+						dout <= '0';
 					else
 						delay_cnt <= delay_cnt + 1;
 						dout <= '1';
@@ -73,17 +80,17 @@ begin
 					if delay_cnt = T_RESET-1 then
 						delay_cnt <= 0;
 						c_state <= LOAD_BIT;
+						dout <= '1';
 					else
 						delay_cnt <= delay_cnt + 1;
-						dout <= '0';
 					end if;
 					
 				when LOAD_BIT =>
 						if frame_buffer(led_idx)(23-bit_idx) = '1' then
-							high_pulse_limit <= T1H;
+							high_pulse_limit <= T1H-1;
 							low_pulse_limit <= T1L;
 						else
-							high_pulse_limit <= T0H;
+							high_pulse_limit <= T0H-1;
 							low_pulse_limit <= T0L;
 						end if;
 						
@@ -97,6 +104,7 @@ begin
 						end if;
 
 						c_state <= BIT_H;
+						dout <= '1';
 						
 				when BIT_H =>
 					if delay_cnt = high_pulse_limit-1 then
@@ -105,20 +113,22 @@ begin
 						c_state <= BIT_L;
 					else
 						delay_cnt <= delay_cnt + 1;
-						dout <= '1';
 					end if;
 					
 				when BIT_L =>
 					if delay_cnt = low_pulse_limit-1 then
 						delay_cnt <= 0;						
-						if led_idx = LED_NUMBER-1 and bit_idx = 0 then
+						if led_idx = LED_NUMBER-1 and bit_idx = 23 then
 							c_state <= IDLE;
+
+							led_idx <= 0;
+							bit_idx <= 0;
 						else
 							c_state <= LOAD_BIT;
 						end if;
+						dout <= '1';
 					else
 						delay_cnt <= delay_cnt + 1;
-						dout <= '0';
 					end if;
 			end case;
 		end if;
